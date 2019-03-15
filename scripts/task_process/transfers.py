dc91ac7cf0d9e022705f86253d2af49de77f98b7
dciangot <diego.ciangottini@cern.ch>
2019-03-15 10:02:41 +0100

    align transfers.py

--- scripts/task_process/transfers.py
+++ scripts/task_process/transfers.py
@@ -2,11 +2,12 @@
 """
 
 """
-from __future__ import division, print_function
+from __future__ import absolute_import, division, print_function
 import json
 import logging
 import os
 
+from TransferInterface import chunks, mark_transferred, mark_failed
 from TransferInterface.RegisterFiles import submit
 from TransferInterface.MonitorTransfers import monitor
 
@@ -16,347 +17,34 @@ if not os.path.exists('task_process/transfers'):
 
 logging.basicConfig(
     filename='task_process/transfers/transfer_inject.log',
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s[%(relativeCreated)6d]%(threadName)s: %(message)s'
 )
 
-if os.path.exists('task_process/rest_filetransfers.txt'): 
-    with open("task_process/rest_filetransfers.txt", "r") as _rest:
-        rest_filetransfers = _rest.readline().split('\n')[0]
-        proxy = os.getcwd() + "/" + _rest.readline()
-        print("Proxy: %s", proxy)
-
-def get_tfc_rules(phedex, site):
-    """
-    Get the TFC regexp for a given site.
-    """
-    tfc_file = None
-    try:
-        phedex.getNodeTFC(site)
-    except Exception as e:
-        logging.exception('PhEDEx exception: %s', e)
-    try:
-        tfc_file = phedex.cacheFileName('tfc',
-                                        inputdata={'node': site})
-    except Exception as e:
-        logging.exception('PhEDEx cache exception: %s', e)
-    return readTFC(tfc_file)
+USER = None
+TASKNAME = None
 
 
-def chunks(l, n):
-    """
-    Yield successive n-sized chunks from l.
-    :param l: list to splitt in chunks
-    :param n: chunk size
-    :return: yield the next list chunk
-    """
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-
-
-def mark_transferred(ids):
-    """
-    Mark the list of files as tranferred
-    :param ids: list of Oracle file ids to update
-    :return: 0 success, 1 failure
-    """
-    try:
-        oracleDB = HTTPRequests(rest_filetransfers,
-                                proxy,
-                                proxy)
-        logging.debug("Marking done %s", ids)
-
-        data = dict()
-        data['asoworker'] = 'asoless'
-        data['subresource'] = 'updateTransfers'
-        data['list_of_ids'] = ids
-        data['list_of_transfer_state'] = ["DONE" for _ in ids]
-
-        oracleDB.post('/filetransfers',
-                      data=encodeRequest(data))
-        logging.debug("Marked good %s", ids)
-    except Exception:
-        logging.exception("Error updating documents")
-        return 1
-    return 0
-
-
-def mark_failed(ids, failures_reasons):
-    """
-    Mark the list of files as failed
-    :param ids: list of Oracle file ids to update
-    :param failures_reasons: list of strings with transfer failure messages
-    :return: 0 success, 1 failure
-    """
-    try:
-        oracleDB = HTTPRequests(rest_filetransfers,
-                                proxy,
-                                proxy)
-        data = dict()
-        data['asoworker'] = 'asoless'
-        data['subresource'] = 'updateTransfers'
-        data['list_of_ids'] = ids
-        data['list_of_transfer_state'] = ["FAILED" for _ in ids]
-        data['list_of_failure_reason'] = failures_reasons
-        data['list_of_retry_value'] = [0 for _ in ids]
-
-        oracleDB.post('/filetransfers',
-                      data=encodeRequest(data))
-        logging.debug("Marked failed %s", ids)
-    except Exception:
-        logging.exception("Error updating documents")
-        return 1
-    return 0
-
-
-class check_states_thread(threading.Thread):
-    """
-    get transfers state per jobid
-    """
-    def __init__(self, threadLock, log, fts, jobid, jobs_ongoing, done_id, failed_id, failed_reasons):
-        """
-
-        :param threadLock:
-        :param log:
-        :param fts:
-        :param jobid:
-        :param jobs_ongoing:
-        :param done_id:
-        :param failed_id:
-        :param failed_reasons:
-        """
-        threading.Thread.__init__(self)
-        self.fts = fts
-        self.jobid = jobid
-        self.jobs_ongoing = jobs_ongoing
-        self.log = log
-        self.threadLock = threadLock
-        self.done_id = done_id
-        self.failed_id = failed_id
-        self.failed_reasons = failed_reasons
-
-    def run(self):
-        """
-        - check if the fts job is in final state (FINISHED, FINISHEDDIRTY, CANCELED, FAILED)
-        - get file transfers states and get corresponding oracle ID from FTS file metadata
-        - update states on oracle
-        """
-
-        self.threadLock.acquire()
-        self.log.info("Getting state of job %s" % self.jobid)
-
-        self.jobs_ongoing.append(self.jobid)
-
-        try:
-            status = self.fts.get("jobs/"+self.jobid)[0]
-        except Exception as ex:
-            self.log.exception("failed to retrieve status for %s " % self.jobid)
-            self.jobs_ongoing.append(self.jobid)
-            self.threadLock.release()
-            return
-
-        self.log.info("State of job %s: %s" % (self.jobid, status["job_state"]))
-
-        # TODO: if in final state get with list_files=True and the update_states
-        if status["job_state"] in ['FINISHED', 'FINISHEDDIRTY', "FAILED", "CANCELED"]:
-            file_statuses = self.fts.get("jobs/%s/files" % self.jobid)[0]
-
-            self.done_id[self.jobid] = []
-            self.failed_id[self.jobid] = []
-            self.failed_reasons[self.jobid] = []
-
-            for file_status in file_statuses:
-                _id = file_status['file_metadata']['oracleId']
-                tx_state = file_status['file_state']
-
-                if tx_state == 'FINISHED':
-                    self.done_id[self.jobid].append(_id)
-                else:
-                    self.failed_id[self.jobid].append(_id)
-                    if file_status['reason']:
-                        self.log.info('Failure reason: ' + file_status['reason'])
-                        self.failed_reasons[self.jobid].append(file_status['reason'])
-                    else:
-                        self.log.exception('Failure reason not found')
-                        self.failed_reasons[self.jobid].append('unable to get failure reason')
-
-        self.threadLock.release()
-
-
-class submit_thread(threading.Thread):
-    """
-
-    """
-    def __init__(self, threadLock, log, context, files, source, jobids, toUpdate):
-        """
-
-        :param threadLock:
-        :param log:
-        :param context:
-        :param files:
-        :param source:
-        :param jobids:
-        :param toUpdate:
-        """
-        threading.Thread.__init__(self)
-        self.log = log
-        self.threadLock = threadLock
-        self.files = files
-        self.source = source
-        self.jobids = jobids
-        self.context = context
-        self.toUpdate = toUpdate
-
-    def run(self):
-        """
-
-        """
-
-        self.threadLock.acquire()
-        self.log.info("Processing transfers from: %s" % self.source)
-
-        # create destination and source pfns for job
-        transfers = []
-        for lfn in self.files:
-            transfers.append(fts3.new_transfer(lfn[0],
-                                               lfn[1],
-                                               metadata={'oracleId': lfn[2]}
-                                               )
-                             )
-        self.log.info("Submitting %s transfers to FTS server" % len(self.files))
-
-        # Submit fts job
-        job = fts3.new_job(transfers,
-                           overwrite=True,
-                           verify_checksum=True,
-                           # TODO: add user DN to metadata
-                           metadata={"issuer": "ASO"},
-                           copy_pin_lifetime=-1,
-                           bring_online=None,
-                           source_spacetoken=None,
-                           spacetoken=None,
-                           max_time_in_queue=600,
-                           retry=3,
-                           retry_delay=600
-                           )
-        # TODO: fts retries?? check delay
-
-        jobid = fts3.submit(self.context, job,
-                            delegation_lifetime=timedelta(hours=48),
-                            delegate_when_lifetime_lt=timedelta(hours=24))
-
-        self.jobids.append(jobid)
-
-        # TODO: manage exception here, what we should do?
-        fileDoc = dict()
-        fileDoc['asoworker'] = 'asoless'
-        fileDoc['subresource'] = 'updateTransfers'
-        fileDoc['list_of_ids'] = [x[2] for x in self.files]
-        fileDoc['list_of_transfer_state'] = ["SUBMITTED" for _ in self.files]
-        fileDoc['list_of_fts_instance'] = ['https://fts3.cern.ch:8446/' for _ in self.files]
-        fileDoc['list_of_fts_id'] = [jobid for _ in self.files]
-
-        self.log.info("Marking submitted %s files" % (len(fileDoc['list_of_ids'])))
-
-        self.toUpdate.append(fileDoc)
-        self.threadLock.release()
-
-
-def submit(phedex, context, toTrans):
-    """
-    submit tranfer jobs
-
-    - group files to be transferred by source site
-    - prepare jobs chunks of max 200 transfers
-    - submit fts job
-
-    :param context: fts client context
-    :param toTrans: [source pfn, destination pfn, oracle file id, source site]
-    :return: list of jobids submitted
-    """
-    threadLock = threading.Lock()
-    threads = []
-    jobids = []
-    to_update = []
-
-    oracleDB = HTTPRequests(rest_filetransfers,
-                            proxy,
-                            proxy)
-
-    sources = list(set([x[3] for x in toTrans]))
-
-    for source in sources:
-
-        ids = [x[2] for x in toTrans if x[3] == source]
-        src_lfns = [x[0] for x in toTrans if x[3] == source]
-        dst_lfns = [x[1] for x in toTrans if x[3] == source]
-
-        sorted_source_pfns = []
-        sorted_dest_pfns = []
-
-        try:
-            for chunk in chunks(src_lfns, 10):
-                unsorted_source_pfns = [[k[1], str(x)] for k, x in phedex.getPFN(source, chunk).items()]
-                for order_lfn in chunk:
-                    for lfn, pfn in unsorted_source_pfns:
-                        if order_lfn == lfn:
-                            sorted_source_pfns.append(pfn)
-                            break
-
-            for chunk in chunks(dst_lfns, 10):
-                unsorted_dest_pfns = [[k[1], str(x)] for k, x in phedex.getPFN(toTrans[0][4], chunk).items()]
-                for order_lfn in chunk:
-                    for lfn, pfn in unsorted_dest_pfns:
-                        if order_lfn == lfn:
-                            sorted_dest_pfns.append(pfn)
-                            break
-        except Exception as ex:
-            logging.error("Failed to map lfns to pfns: %s", ex)
-            mark_failed(ids, ["Failed to map lfn to pfn: " + str(ex) for _ in ids])
-
-        source_pfns = sorted_source_pfns
-        dest_pfns = sorted_dest_pfns
-
-        tx_from_source = [[x[0], x[1], x[2], source] for x in zip(source_pfns, dest_pfns, ids)] 
-
-        # TODO: registration on Rucio
-        from rucio.client.replicaclient import ReplicaClient
-
-        R = ReplicaClient()
-
-        R.add_replicas(rse=OPTIONS.rse, files=source_lfns)
-
-        for files in chunks(tx_from_source, 200):
-            thread = submit_thread(threadLock,
-                                   logging,
-                                   context,
-                                   files,
-                                   source,
-                                   jobids,
-                                   to_update)
-            thread.start()
-            threads.append(thread)
-
-    for t in threads:
-        t.join()
-
-    for fileDoc in to_update:
-        _ = oracleDB.post('/filetransfers',
-                          data=encodeRequest(fileDoc))
-        logging.info("Marked submitted %s files", fileDoc['list_of_ids'])
-
-    return jobids
-
-
-def perform_transfers(inputFile, lastLine, _lastFile, context, phedex):
+def perform_transfers(inputFile, lastLine):
     """
     get transfers and update last read line number
     :param inputFile:
     :param lastLine:
     :return:
     """
-    transfers = []
-    logging.info("starting from line: %s", lastLine)
+    proxy = None
+    if os.path.exists('task_process/rest_filetransfers.txt'): 
+        with open("task_process/rest_filetransfers.txt", "r") as _rest:
+            rest_filetransfers = _rest.readline().split('\n')[0]
+            proxy = os.getcwd() + "/" + _rest.readline()
+            logging.info("Proxy: %s", proxy)
+            os.environ["X509_USER_PROXY"] = proxy
+
+    if not proxy:
+        logging.info('No proxy available yet - waiting for first post-job')
+        return None
+
+    logging.info("starting from line: %s" % lastLine)
 
     file_to_submit = []
     to_submit_columns = ["source_lfn",
@@ -383,7 +71,7 @@ def perform_transfers(inputFile, lastLine, _lastFile, context, phedex):
             try:
                 lastLine += 1
                 doc = json.loads(_data)
-            except Exception:
+            except:
                 continue
             for column in to_submit_columns:
                 if column not in ['checksums']:
@@ -400,28 +88,16 @@ def perform_transfers(inputFile, lastLine, _lastFile, context, phedex):
                 'rest': rest_filetransfers}
 
     if len(transfers) > 0:
-        if not direct:
-            try:
-                submit((transfers, to_submit_columns), job_data, logging)
-                # TODO: send to dashboard
-            except Exception:
-                logging.exception('Submission process failed.')
+        try:
+            submit((transfers, to_submit_columns), job_data, logging)
+            # TODO: send to dashboard
+        except:
+            logging.exception('Submission process failed.')
 
-            with open("task_process/transfers/last_transfer_new.txt", "w+") as _last:
-                _last.write(str(lastLine))
+        with open("task_process/transfers/last_transfer_new.txt", "w+") as _last:
+            _last.write(str(lastLine))
 
-            os.rename("task_process/transfers/last_transfer_new.txt", "task_process/transfers/last_transfer.txt")
-        elif direct:
-            try:
-                submit((transfers, to_submit_columns), job_data, logging, direct=True)
-                # TODO: send to dashboard
-            except Exception:
-                logging.exception('Submission process failed.')
-
-            with open("task_process/transfers/last_transfer_direct_new.txt", "w+") as _last:
-                _last.write(str(lastLine))
-
-            os.rename("task_process/transfers/last_transfer_direct_new.txt", "task_process/transfers/last_transfer_direct.txt")
+        os.rename("task_process/transfers/last_transfer_new.txt", "task_process/transfers/last_transfer.txt")
 
     return user, taskname
 
@@ -431,9 +107,9 @@ def monitor_manager(user, taskname):
 
     """
     proxy = None
-    if os.path.exists('task_process/rest_filetransfers.txt'):
+    if os.path.exists('task_process/rest_filetransfers.txt'): 
         with open("task_process/rest_filetransfers.txt", "r") as _rest:
-            _rest.readline().split('\n')[0]
+            _ = _rest.readline().split('\n')[0]
             proxy = os.getcwd() + "/" + _rest.readline()
             logging.info("Proxy: %s", proxy)
             os.environ["X509_USER_PROXY"] = proxy
@@ -444,47 +120,13 @@ def monitor_manager(user, taskname):
 
     try:
         monitor(user, taskname, logging)
-    except Exception:
+    except:
         logging.exception('Monitor process failed.')
 
     return 0
 
-        try:
-            for jobID, _ in done_id.iteritems():
-                logging.info('Marking job %s files done and %s files failed for job %s', len(done_id[jobID]), len(failed_id[jobID]), jobID)
 
-                if len(done_id[jobID]) > 0:
-                    doneReady = mark_transferred(done_id[jobID])
-                else:
-                    doneReady = 0
-                if len(failed_id[jobID]) > 0:
-                    failedReady = mark_failed(failed_id[jobID],
-                                              failed_reasons[jobID])
-                else:
-                    failedReady = 0
-
-                if doneReady == 0 and failedReady == 0:
-                    jobs_done.append(jobID)
-                    jobs_ongoing.remove(jobID)
-                else:
-                    jobs_ongoing.append(jobID)
-        except Exception:
-            logging.exception('Failed to update states')
-    else:
-        logging.warning('No FTS job ID to monitor yet')
-
-    with open("task_process/transfers/fts_jobids_new.txt", "w+") as _jobids:
-        for line in jobs_ongoing:
-            logging.info("Writing: %s", line)
-            _jobids.write(line+"\n")
-
-    os.rename("task_process/transfers/fts_jobids_new.txt",
-              "task_process/transfers/fts_jobids.txt")
-
-    return jobs_ongoing
-
-
-def submission_manager(phedex, context):
+def submission_manager():
     """
 
     """
@@ -495,23 +137,12 @@ def submission_manager(phedex, context):
             last_line = int(read)
             logging.info("last line is: %s", last_line)
             _last.close()
-    
-    # TODO: if the following fails check not to leave a corrupted file
-    r = perform_transfers("task_process/transfers.txt",
-                          last_line)
-
-    if os.path.exists('task_process/transfers/last_transfer_direct.txt'):
-        with open("task_process/transfers/last_transfer_direct.txt", "r") as _last:
-            read = _last.readline()
-            last_line = int(read)
-            logging.info("last line is: %s", last_line)
-            _last.close()
 
     # TODO: if the following fails check not to leave a corrupted file
-    r_d = perform_transfers("task_process/transfers_direct.txt",
-                            last_line, direct=True)
+    exit = perform_transfers("task_process/transfers.txt",
+                             last_line)
 
-    return r
+    return exit
 
 
 def algorithm():
@@ -528,18 +159,15 @@ def algorithm():
     - append new fts job ids to fts_jobids.txt
     """
 
-    user = None
     try:
-        phedex = PhEDEx(responseType='xml',
-                        httpDict={'key': proxy, 'cert': proxy, 'pycurl':True})
-    except Exception as e:
-        logging.exception('PhEDEx exception: %s', e)
-        return
+        user, taskname = submission_manager()
+    except:
+        logging.exception('Submission proccess failed.')
 
-    jobs_ongoing = state_manager(fts)
-    new_jobs = submission_manager(phedex, context)
-
-    logging.debug("Transfer jobs ongoing: %s, %s ", jobs_ongoing, new_jobs)
+    try:
+        monitor_manager(user, taskname)
+    except:
+        logging.exception('Monitor proccess failed.')
 
     return
 
